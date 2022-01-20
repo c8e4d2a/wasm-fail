4298adc65b0e9c887c65f511721d4bd2d4dea1f9
Perlie Christal <c8e4d2a@protonmail.com>
2022-01-21 00:40:41 +0100

    esbuild wasm plugin

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import { terser } from 'rollup-plugin-terser'
 import svelte from 'rollup-plugin-svelte'
 import livereload from 'rollup-plugin-livereload'
-import { wasm } from '@rollup/plugin-wasm'
+import esbuild from 'rollup-plugin-esbuild'
 
 const { distDir, buildDir } = appConfig
 const production = process.env['NODE_ENV'] === 'production'
@@ -24,6 +24,45 @@ export default {
     chunkFileNames: `[name]${(production && '-[hash]') || ''}.js`,
   },
   plugins: [
+    //https://esbuild.github.io/plugins/#webassembly-plugin
+    esbuild({
+      plugins: [{
+        name: 'wasm',
+        setup(build) {
+          let path = require('path')
+          let fs = require('fs')
+
+          build.onResolve({ filter: /\.wasm$/ }, args => {
+            // If this is the import inside the stub module, import the
+            // binary itself. Put the path in the "wasm-binary" namespace
+            // to tell our binary load callback to load the binary file.
+            if (args.namespace === 'wasm-stub') {
+              return {
+                path: args.path,
+                namespace: 'wasm-binary',
+              }
+            }
+      
+            if (args.resolveDir === '') {
+              return // Ignore unresolvable paths
+            }
+            return {
+              path: path.isAbsolute(args.path) ? args.path : path.join(args.resolveDir, args.path),
+              namespace: 'wasm-stub',
+            }
+          })
+          build.onLoad({ filter: /.*/, namespace: 'wasm-stub' }, async (args) => ({
+            contents: `import wasm from ${JSON.stringify(args.path)}
+              export default (imports) =>
+                WebAssembly.instantiate(wasm, imports).then(
+                  result => result.instance.exports)`,
+          }))
+          build.onLoad({ filter: /.*/, namespace: 'wasm-binary' }, async (args) => ({
+            contents: await fs.promises.readFile(args.path),
+            loader: 'binary',
+          }))
+        },}],
+    }),
     svelte({
       preprocess: [
         svelteWindicss({
@@ -39,7 +78,6 @@ export default {
       dedupe: (importee) => !!importee.match(/svelte(\/|$)/),
     }),
     commonjs(),
-    wasm(),
     production && terser(),
     !production && livereload(distDir), // refresh entire window when code is updated
   ],
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -2,7 +2,7 @@
     import { Address } from "ergo-lib-wasm-browser";
 
     const addr = "8fc4fc070cc786fa723cbfac54e2d71094daf275ebe1088caf602ddcefc95d88";
-    a =  Address.from_mainnet_str(addr).to_ergo_tree().to_base16_bytes()
+    a =  Address.from_mainnet_str(addr).to_ergo_tree().to_base16_bytes();
 </script>
 
 <h1>App?!</h1>
